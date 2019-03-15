797c66cc5b620ff45c42ef71fa16ae0c1c155e3b
Sinjoy Saha <sinjoysaha555@gmail.com>
2019-03-16 00:24:41 +0530

    Adding atoi

---
+++ chapter 3/atoi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAXLINE 100
+int atoi(char[]);
+
+int main(void)
+{
+    char s[MAXLINE];
+    printf("Enter string: ");
+    scanf("%s", &s);
+    printf("%d\n", atoi(s));
+    return 0;
+}
+/* atoi: convert s to integer; version 2 */
+int atoi(char s[])
+{
+    int i, n, sign;
+    for(i=0; isspace(s[i]); i++);
+    sign = (s[i]=='-') ? -1:1;
+    if(s[i]=='+' || s[i]=='-')
+        i++;
+    for (n=0; isdigit(s[i]); i++)
+        n = 10*n + (s[i]-'0');
+    return sign*n;
+}
