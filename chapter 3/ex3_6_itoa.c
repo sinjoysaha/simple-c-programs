83eabd1c3e8643f96a659f77720fd06e02f69fdb
sinjoysaha <sinjoysaha555@gmail.com>
2019-03-18 00:17:36 +0530

    Added ex3_6_itoa

--- chapter 3/ex3_6_itoa.c
+++ chapter 3/ex3_6_itoa.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #define MAXLINE 100
-void itoa(int, char[]);
+void itoa(int, char[], int);
 void reverse(char[]);
 int main(void)
 {
     char s[MAXLINE];
-    int n;
+    int n, w;
     printf("Enter number: ");
     scanf("%d", &n);
-    itoa(n,s);
+    printf("Enter min field width: ");
+    scanf("%d", &w);
+    itoa(n,s,w);
     printf("%s\n", s);
     return 0;
 }
-/* itoa: convert integer to string; version 2*/
-void itoa(int n, char s[])
+/* itoa: convert integer to string; version 3; with padding */
+void itoa(int n, char s[], int w)
 {
     int i, sign;
     sign = (n<0) ? -1:1;
@@ -23,9 +25,15 @@ void itoa(int n, char s[])
     {
         s[i++] = (n%10)*sign + '0';
         n/= 10;
+        w--;
     }while((n*sign)>0);
     if (sign < 0)
+    {
         s[i++] = '-';
+        w--;
+    }
+    while(w-->0)
+        s[i++] = ' ';
     s[i] = '\0';
     reverse(s);
 }
